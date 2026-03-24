8055640d446e2d11b89d702f83c997cc8503b33a
mohammed hamdi <mohammedhamdi262003@gmail.com>
2026-03-24 20:39:20 +0200

    Add files via upload
    add vaildation for name db

--- createDB.sh
+++ createDB.sh
@@ -1,18 +1,45 @@
-#!/bin/bash
+createDatabase(){
+reserved_words=("select" "create" "drop" "delete" "insert" "update" "where" "table" "database")
 
-create_DB() {
 
-	echo ""
-	read -p "Enter a name for the data base: " db_name
+while true
+do
+    read -p "Enter database name: " dbname
 
-	if [ -z "$db_name" ]; then
-		echo "Database name can't be empty!"
-		return
-	else
-		mkdir $db_name
-		echo "Database '$db_name' is Created"
-		echo ""
-	fi
+    if ! [[ $dbname =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
+    then
+        echo "Invalid name ❌"
+        continue
+    fi
+
+    is_reserved=false
+
+    for word in "${reserved_words[@]}"
+    do
+        if [[ "$dbname" == "$word" ]]
+        then
+            is_reserved=true
+            break
+        fi
+    done
+
+    if [[ $is_reserved == true ]]
+    then
+        echo "Invalid name ❌ reserved keyword"
+        continue
+    fi
+
+    if [[ -d "databases/$dbname" ]]
+    then
+        echo "Database already exists ❌"
+        continue
+    fi
+
+    mkdir "databases/$dbname"
+    echo "Database created successfully ✅"
+    break
+
+done
 }
 
-create_DB
+createDatabase
